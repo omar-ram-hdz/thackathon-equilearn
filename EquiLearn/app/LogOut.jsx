642e1d627cc50db6dc8f931e97d819389e8a1040
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-05-10 16:40:59 -0600

    Add UI Components

--- EquiLearn/app/LogOut.jsx
+++ EquiLearn/app/LogOut.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import {
   View,
   StyleSheet,
-  SafeAreaView,
   TextInput,
   TouchableWithoutFeedback,
   Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
 import HeaderForm from '../components/form/HeaderForm.jsx';
 import OwnText from '../components/OwnText.jsx';
@@ -16,6 +18,7 @@ import { router } from 'expo-router';
 
 const LogOut = () => {
   const [mail, setMail] = useState('');
+  const [name, setName] = useState('');
   const [pass, setPass] = useState('');
 
   const [fontsLoaded] = useFonts({
@@ -24,78 +27,95 @@ const LogOut = () => {
     'Ubuntu-Regular': require('../assets/fonts/Ubuntu-Regular.ttf'),
   });
   return (
-    <View style={styles.container}>
-      <HeaderForm />
-      <View style={styles.ctaText}>
-        <OwnText primary bold big>
-          Estudia. Aprende.
-        </OwnText>
-        <OwnText big addStyle={{ flexDirection: 'row' }}>
-          Iniciar Sesión en{' '}
-          <OwnText bold big>
-            EquiLearn
-          </OwnText>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View>
+          <HeaderForm />
+          <View style={styles.ctaText}>
+            <OwnText primary bold big>
+              Estudia. Aprende.
+            </OwnText>
+            <OwnText big addStyle={{ flexDirection: 'row' }}>
+              Registrarse en{' '}
+              <OwnText bold big>
+                EquiLearn
+              </OwnText>
+            </OwnText>
+          </View>
+          <View style={styles.form}>
+            <View>
+              <OwnText big>Correo</OwnText>
+              <TextInput
+                style={styles.input}
+                onChangeText={(nt) => setMail(nt)}
+                placeholder="hola@equilearn.com"
+                placeholderTextColor={Theme.colors.gray}
+                value={mail}
+                keyboardType="email"
+              />
+            </View>
+
+            <View>
+              <OwnText big>Nombre</OwnText>
+              <TextInput
+                style={styles.input}
+                onChangeText={(nt) => setName(nt)}
+                placeholder="Pedro Pascal"
+                placeholderTextColor={Theme.colors.gray}
+                value={name}
+                keyboardType="default"
+              />
+            </View>
+
+            <View>
+              <OwnText big>Contraseña</OwnText>
+              <TextInput
+                style={styles.input}
+                onChangeText={(nt) => setPass(nt)}
+                placeholder="secret1234"
+                placeholderTextColor={Theme.colors.gray}
+                value={pass}
+              />
+            </View>
+
+            <View style={{ marginTop: 50 }}>
+              <OwnButton
+                onP={() =>
+                  Alert.alert('Mensaje de EquiLearn', 'Hola! esto ya funciona')
+                }
+              >
+                Registrarse
+              </OwnButton>
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+      <View style={{ marginTop: 20 }}>
+        <OwnText medium center>
+          ¿Ya tienes cuenta?{' '}
+          <TouchableWithoutFeedback onPress={() => router.navigate('LogIn')}>
+            <OwnText medium primary bold>
+              Iniciar Sesión
+            </OwnText>
+          </TouchableWithoutFeedback>
         </OwnText>
       </View>
-      {/* <HeaderForm />
-      <View style={styles.ctaText}>
-        <OwnText primary bold big>
-          Estudia. Aprende.
-        </OwnText>
-        <OwnText bold big>
-          Iniciar Sesión en{' '}
-          <OwnText primary bold big>
-            EquiLearn
-          </OwnText>
-        </OwnText>
-      </View>
-      <SafeAreaView style={styles.form}>
-        <View>
-          <OwnText big>Correo</OwnText>
-          <TextInput
-            style={styles.input}
-            onChangeText={(nt) => setMail(nt)}
-            placeholder="hola@equilearn.com"
-            placeholderTextColor={Theme.colors.gray}
-            value={mail}
-          />
-        </View>
-        <View>
-          <OwnText big>Contraseña</OwnText>
-          <TextInput
-            style={styles.input}
-            onChangeText={(nt) => setPass(nt)}
-            placeholder="secret1234"
-            placeholderTextColor={Theme.colors.gray}
-            value={pass}
-          />
-        </View>
-        <OwnButton onP={() => Alert.alert('Mensaje', 'Hola !')}>
-          {'Iniciar Sesión'}
-        </OwnButton>
-      </SafeAreaView>
-      <OwnText medium>
-        ¿No tienes cuenta?{' '}
-        <TouchableWithoutFeedback onPress={() => router.navigate('LogIn')}>
-          <OwnText medium primary bold>
-            Registrarse
-          </OwnText>
-        </TouchableWithoutFeedback>
-      </OwnText> */}
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
     paddingHorizontal: 20,
   },
   ctaText: {
     marginTop: 25,
   },
   form: {
-    marginTop: 150,
+    marginTop: 20,
     marginBottom: 50,
   },
   inputText: {
