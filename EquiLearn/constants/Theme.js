13c23a51175ece7a14fa1e49ba5ab770c3bfa9a6
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-05-10 09:56:50 -0600

    Add configuration UI values

---
+++ EquiLearn/constants/Theme.js
@@ -0,0 +1,29 @@
+export const Theme = {
+  colors: {
+    blackPrimary: '#0C0636',
+    bluePrimary: '#095169',
+    black: '#323232',
+    blueTertiary: '#059B9A',
+    green: '#53BA83',
+    greenLow: '#53BA83',
+    blackIntense: '#010101',
+    gray: '#777777',
+  },
+  sizes: {
+    text: {
+      big: 30,
+      medium: 20,
+      low: 15,
+    },
+    radius: {
+      primary: 20,
+      medium: 10,
+      exaggerated: 50,
+    },
+  },
+  fonts: {
+    bold: 'Ubuntu-Bold',
+    normal: 'Ubuntu-Regular',
+    italic: 'Ubuntu-Italic',
+  },
+};
