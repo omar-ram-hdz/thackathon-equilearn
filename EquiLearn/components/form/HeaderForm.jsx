b14f060b03e5389f5786ee5a2c0fddd36dcfc720
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-05-10 09:56:23 -0600

    Add component Header from Forms

---
+++ EquiLearn/components/form/HeaderForm.jsx
@@ -0,0 +1,27 @@
+import { View, StyleSheet } from 'react-native';
+import OwnText from '../OwnText.jsx';
+import BigIcon from '../BigIcon.jsx';
+
+const HeaderForm = ({ addStyles }) => {
+  return (
+    <View style={{ ...styles.container, ...addStyles }}>
+      <BigIcon
+        style={{ width: 40, height: 40, objectFit: 'scale-down' }}
+        alt="EquiLearn Icon"
+      />
+      <OwnText primary bold big>
+        EquiLearn
+      </OwnText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flex: 0,
+  },
+});
+export default HeaderForm;
