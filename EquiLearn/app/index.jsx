a0ded2dc37b0f6b63fd2a1d6080df53cdc7a35f0
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-05-13 18:55:04 -0600

    Add Icon on splash

--- EquiLearn/app/index.jsx
+++ EquiLearn/app/index.jsx
@@ -1,31 +1,80 @@
-import { Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
 import * as ss from 'expo-secure-store';
 import { router } from 'expo-router';
 import { useEffect, useState } from 'react';
+import { Theme } from '../constants/Theme';
+import BigIcon from '../components/BigIcon.jsx';
+import OwnText from '../components/OwnText';
+import { useFonts } from 'expo-font';
 
 const Index = () => {
   const [userId, setUserId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [fontsLoaded] = useFonts({
+    'Ubuntu-Bold': require('../assets/fonts/Ubuntu-Bold.ttf'),
+    'Ubuntu-Italic': require('../assets/fonts/Ubuntu-Italic.ttf'),
+    'Ubuntu-Regular': require('../assets/fonts/Ubuntu-Regular.ttf'),
+  });
   useEffect(() => {
     ss.getItemAsync('userId')
       .then((res) => {
-        setUserId(res);
         if (res === null) {
-          router.navigate('/LogOut');
+          setTimeout(() => {
+            setIsLoading(false);
+            setUserId(res);
+            router.navigate('/LogOut');
+          }, 2000);
         } else {
           // Index entry
         }
       })
       .catch((e) => {
-        setError(true);
+        setError(e);
+      })
+      .finally(() => {
+        /*setIsLoading(false)*/
       });
   }, []);
   return (
-    <View>
-      {userId && <Text>Cargando...</Text>}
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor={Theme.colors.black}
+        barStyle="light-content "
+      />
+      <BigIcon addStyle={styles.icon} />
+      {isLoading && (
+        <View>
+          <OwnText bold big primary>
+            Cargando...
+          </OwnText>
+          <ActivityIndicator size="large" color={Theme.colors.bluePrimary} />
+        </View>
+      )}
       {error && <Text style={{ color: 'f14' }}>Error</Text>}
-    </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Theme.colors.black,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    borderRadius: 10,
+    width: 400,
+    height: 100,
+  },
+});
 
 export default Index;
