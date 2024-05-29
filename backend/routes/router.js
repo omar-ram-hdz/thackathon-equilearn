51ffa20608f7cbe001fa6d11a9e9f9ded38d6486
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-05-28 21:25:53 -0600

    refactor: Add api integration

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
+import { createAPIRouter } from './api.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const createRouter = () => {
   const mainRouter = Router();
-  
+
+  mainRouter.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/index.min.html'));
+  });
+
+  mainRouter.use('/api', createAPIRouter());
+
   return mainRouter;
 };
