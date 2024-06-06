a9b965762f5dcb4de1bda8fd642196319a84c349
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-06-06 10:15:17 -0600

    feature: add courses petitions

--- EquiLearn/helpers/OwnAxios.js
+++ EquiLearn/helpers/OwnAxios.js
@@ -26,13 +26,42 @@ export const UserAxios = {
       };
     }
   },
-  post: async (url, pass, email, full_name, signal) => {
+  post: async (url, pass, email, full_name, grade, signal) => {
     try {
       let res = await axios.post(
           `${url}`,
-          { full_name, email, pass },
+          { full_name, email, pass, grade },
           { signal },
         ),
+        data = await res.data;
+      return data;
+    } catch (err) {
+      let message = err.response.statusText || 'Ocurrió un error';
+      return {
+        status: false,
+        err: `Error: ${err.response.status || 400} -- ${message}`,
+      };
+    }
+  },
+};
+
+export const CourseAxios = {
+  getAll: async (url, grade, signal) => {
+    try {
+      let res = await axios.get(`${url}/${grade}`, { signal }),
+        data = await res.data;
+      return data;
+    } catch (err) {
+      let message = err.response.statusText || 'Ocurrió un error';
+      return {
+        status: false,
+        err: `Error: ${err.response.status || 400} -- ${message}`,
+      };
+    }
+  },
+  get: async (url, id, signal) => {
+    try {
+      let res = await axios.get(`${url}/${id}`, { signal }),
         data = await res.data;
       return data;
     } catch (err) {
