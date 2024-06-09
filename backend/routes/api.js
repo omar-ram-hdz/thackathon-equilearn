7c60e780730b941ef93e0661663e37c68799dbab
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-06-08 19:49:12 -0600

    feature: Add course and lesson endpoints

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user_controller.js';
 import { CourseController } from '../controllers/course_controller.js';
+import { ContentController } from '../controllers/content_controller.js';
+import { TopicModel } from '../models/topic_model.js';
+import { SubTopicModel } from '../models/subtopic_model.js';
 import { UserModel } from '../models/user_model.js';
 import { CourseModel } from '../models/course_model.js';
 
@@ -8,13 +11,17 @@ export const createAPIRouter = () => {
   const api = Router();
   const userController = new UserController(UserModel);
   const courseController = new CourseController(CourseModel);
+  const contentController = new ContentController(TopicModel, SubTopicModel);
   // User endpoints
   api.post('/user', userController.create);
   api.get('/user/:id', userController.getById);
   api.get('/user/:email/:pass', userController.get);
 
   // Course endpoints
-  api.get('/courses', courseController.getAll);
+  api.get('/courses/:grade', courseController.getAll);
   api.get('/course/:id', courseController.get);
+  api.get('/course/:id/content', contentController.get);
+  api.get('/course/:id/content-partial', contentController.getPartial);
+  api.get('/lesson/:id', contentController.getLesson);
   return api;
 };
